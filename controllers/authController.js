0a8244a7edc3401c25ad7c02d4a34837d95ebd85
Samet Candan <c._samet@hotmail.com>
2025-02-25 23:33:45 +0300

    categories

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const Category = require("../models/Category");
 
 exports.createUser = async (req, res) => {
   try {
@@ -64,5 +65,8 @@ exports.logoutUser = (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.userID });
-  res.status(200).render("dashboard", { pageName: "dashboard", user });
+  const categories = await Category.find();
+  res
+    .status(200)
+    .render("dashboard", { pageName: "dashboard", user, categories });
 };
