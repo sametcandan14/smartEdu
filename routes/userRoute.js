8a008972d81cc94ebd3b474e41448a3e28474d51
Samet Candan <c._samet@hotmail.com>
2025-03-03 06:35:49 +0300

    validation

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 
 const {
   createUser,
@@ -7,11 +8,30 @@ const {
   getDashboardPage,
 } = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const User = require("../models/User");
 
 const router = express.Router();
 
 //Routes
-router.route("/signup").post(createUser);
+router.route("/signup").post(
+  [
+    body("name").not().isEmpty().withMessage("Please Enter Your Name"),
+
+    body("email")
+      .isEmail()
+      .withMessage("Please Enter Valid Email")
+      .custom((userEmail) => {
+        return User.findOne({ email: userEmail }).then((user) => {
+          if (user) {
+            return Promise.reject("Email already exist!");
+          }
+        });
+      }),
+
+    body("password").not().isEmpty().withMessage("Please Enter A Password"),
+  ],
+  createUser
+);
 
 router.route("/login").post(loginUser);
 
